6173142587791a4b6edbe9607461d49f28d9e58d
nazennash <nazennash120@gmail.com>
2024-11-07 10:57:04 +0300

    subscribe

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Customer } from "../components/Customers/Customer";
 import { Dishes } from "../components/Dishes/Dishes";
 import { Experts } from "../components/Experts/Experts";
 import { Header } from "../components/Header/Header";
+import { Promo } from "../components/Promo/Promo";
 import { Welcome } from "../components/Welcome/Welcome";
 
 export const Home = () => {
@@ -12,6 +13,10 @@ export const Home = () => {
       <Welcome />
       <Experts />
       <Customer />
+      <Promo />
+      <br />
+      <br />
+      <div className="mt-30"></div>
     </div>
   );
 };
